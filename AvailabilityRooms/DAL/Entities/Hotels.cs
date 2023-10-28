71287f55ad7f4ac1eda61345084556fe15116b9a
samalxMia <126675804+samalxMia@users.noreply.github.com>
2023-10-28 17:40:01 -0500

    Creation of DAL layers, Domain and entities, Hotels and Rooms, and AuditBase.

---
+++ AvailabilityRooms/DAL/Entities/Hotels.cs
@@ -0,0 +1,15 @@
+﻿namespace AvailabilityRooms.DAL.Entities
+{
+    public class Hotels : AuditBase
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public int Stars { get; set; }
+        public string City { get; set; }
+
+
+        public List<Rooms>? Rooms { get; set; }
+    }
+}
---
+++ AvailabilityRooms/DAL/Entities/Rooms.cs
@@ -0,0 +1,14 @@
+﻿namespace AvailabilityRooms.DAL.Entities
+{
+    public class Rooms : AuditBase
+    {
+
+        public Guid Id { get; set; }
+        public int Number { get; set; }
+        public int MaxGuests { get; set; }
+        public bool Availability { get; set; }
+
+        public Guid HotelId { get; set; } // Clave foránea para relacionar con Hotel
+        public Hotels hotels { get; set; }
+    }
+}
